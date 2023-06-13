f3d7999201e1ef9f19b3004b5bc7848169272af0
queena23 <queena.wu232@gmail.com>
2023-06-13 15:16:00 -0700

    implemented to-do-list fucntions (need to add css)

---
+++ tdl.js
@@ -0,0 +1,39 @@
+const todoList = [];
+
+displayList();
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const dateInputElement = document.querySelector('.js-date-input');
+    const dueDate = dateInputElement.value;
+    const name = inputElement.value;
+    if (name === '' || dueDate === ''){
+        document.querySelector('.js-result').innerHTML = `Please fill up everything`;    
+    } else {
+        document.querySelector('.js-result').innerHTML = ``;    
+        todoList.push({
+            name,
+            dueDate
+        });
+        inputElement.value = '';
+        dateInputElement.value = '';
+    }
+    displayList();
+}
+
+function deleteTodo(i) {
+    todoList.splice(i, 1);
+    displayList();
+}
+function displayList() {
+    let todoListHTML = '';
+    for (let index = 0; index < todoList.length; index++) {
+        const todoObject = todoList[index];
+        const {name, dueDate} = todoObject;
+        const html = 
+        `<p class = 'js-todo'>${name} ${dueDate}</p>
+        <button onclick = "deleteTodo(${index})" class = 'delete-button'>Delete </button>`;
+        todoListHTML += html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
